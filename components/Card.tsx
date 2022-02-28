aea2e519f0c1f4c88309f2acf595de4dd8615e72
anohene1 <anoheneisaac@gmail.com>
2022-02-28 13:56:25 +0000

    refactor card to own component

---
+++ components/Card.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { urlFor } from '../sanity'
+import Link from 'next/link'
+import { Post } from '../typings'
+
+interface Props {
+  post: Post
+}
+
+function Card({ post }: Props) {
+  return (
+    <Link href={`/posts/${post.slug.current}`}>
+      <div className="border rounded-lg group cursor-pointer overflow-hidden">
+        <img
+          className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+          src={urlFor(post.mainImage).url()!}
+          alt={post.title}
+        />
+        <div className="flex justify-between p-5 bg-white">
+          <div>
+            <p className="text-lg font-bold">{post.title}</p>
+            <p className="text-xs">
+              {post.description} by {post.author.name}
+            </p>
+          </div>
+          <img
+            src={urlFor(post.author.image).url()!}
+            alt={post.author.name}
+            className="h-12 w-12 rounded-full"
+          />
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default Card
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Banner from '../components/Banner'
 import { sanityClient, urlFor } from '../sanity'
 import { Post } from '../typings'
 import Link from 'next/link'
+import Card from '../components/Card'
 
 interface Props {
   posts: [Post]
@@ -23,28 +24,7 @@ export default function Home({ posts }: Props) {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
         {posts.map((post) => (
-          <Link href={`/posts/${post.slug.current}`}>
-            <div className="border rounded-lg group cursor-pointer overflow-hidden">
-              <img
-                className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
-                src={urlFor(post.mainImage).url()!}
-                alt={post.title}
-              />
-              <div className="flex justify-between p-5 bg-white">
-                <div>
-                  <p className="text-lg font-bold">{post.title}</p>
-                  <p className="text-xs">
-                    {post.description} by {post.author.name}
-                  </p>
-                </div>
-                <img
-                  src={urlFor(post.author.image).url()!}
-                  alt={post.author.name}
-                  className="h-12 w-12 rounded-full"
-                />
-              </div>
-            </div>
-          </Link>
+          <Card post={post} />
         ))}
       </div>
     </div>
