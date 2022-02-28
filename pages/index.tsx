f8ad5543038d9334e65d1af6415b6098b046669d
anohene1 <anoheneisaac@gmail.com>
2022-02-28 13:53:03 +0000

    add header, banner and render post cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,77 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Header from '../components/Header'
+import Banner from '../components/Banner'
+import { sanityClient, urlFor } from '../sanity'
+import { Post } from '../typings'
+import Link from 'next/link'
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post]
+}
+
+export default function Home({ posts }: Props) {
+  console.log(posts)
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <p>This is medium</p>
+      <Header />
+      <Banner />
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
+        {posts.map((post) => (
+          <Link href={`/posts/${post.slug.current}`}>
+            <div className="border rounded-lg group cursor-pointer overflow-hidden">
+              <img
+                className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                src={urlFor(post.mainImage).url()!}
+                alt={post.title}
+              />
+              <div className="flex justify-between p-5 bg-white">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+                <img
+                  src={urlFor(post.author.image).url()!}
+                  alt={post.author.name}
+                  className="h-12 w-12 rounded-full"
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
 
-export default Home
+export async function getServerSideProps() {
+  const query = `
+  *[_type == "post"] {
+    _id,
+    title,
+    body,
+    mainImage,
+  author-> {
+  name,
+  image
+},
+description,
+slug
+   
+  }`
+
+  const posts = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
